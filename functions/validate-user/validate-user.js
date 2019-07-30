af08a4f2d5f5b80fadbb33eeb882086c318c37fe
vnovick <vnovick@gmail.com>
2019-07-30 12:01:11 +0300

    validate user

---
+++ functions/validate-user/validate-user.js
@@ -0,0 +1,32 @@
+const fetch = require("node-fetch");
+
+exports.handler = async (event, context) => {
+  const { identity, user } = context.clientContext;
+  const userID = user.sub;
+  const userUrl = `${identity.url}/admin/users/{${userID}}`;
+  const adminAuthHeader = "Bearer " + identity.token;
+
+  try {
+    return fetch(userUrl, {
+      method: "GET",
+      headers: { Authorization: adminAuthHeader }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log("data", JSON.stringify(data));
+        return { statusCode: 204 };
+      })
+      .catch(e => {
+        console.log("Failed to get user! 500! Internal.");
+        return {
+          statusCode: 500,
+          body: "Internal Server Error: " + e
+        };
+      });
+  } catch (e) {
+    console.log("GOT HERE! 500! outer");
+    return { statusCode: 500, body: "Internal Server Error: " + e };
+  }
+};
